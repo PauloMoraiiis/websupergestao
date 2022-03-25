fb3fe52ecc0016879ba161bdffc9641c7717d88f
PauloMoraiiis <paulodevmorais@gmail.com>
2022-03-25 17:39:58 -0300

    Customizando feedback do form_contato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -17,15 +17,29 @@ class ContatoController extends Controller
 
     public function salvar(Request $request) {
 
-        //realizar a validação dos dados recebidos pelo request
-        $request->validate([
-            'nome' => 'required|min:3|max:40',
+        $regras = [
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
-            'email' => 'required',
-            'motivo_contato' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
+        ];
 
-        //SiteContato::create($request->all());
+        $feedback = [
+            'nome.required' => 'O campo nome precisa ser preenchido',
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome pode ter no máxímo 40 caracteres',
+            'nome.unique' => 'O nome informado já está incluso',
+            'email.email' => 'O e-mail informado não é válido',
+            'mensagem.max' => 'A mensagem deve ter no mínimo 2000 caracteres',
+            'required' => 'o campo :attribute deve ser preenchido'
+        ];
+
+        //realizar a validação dos dados recebidos pelo request
+        $request->validate($regras, $feedback);
+
+        SiteContato::create($request->all());
+
+        return redirect()->route('site.index');
     }
 }
