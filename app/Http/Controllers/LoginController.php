256b103049a83ce4808e3e18135e3b9be7f87c70
PauloMoraiiis <paulodevmorais@gmail.com>
2022-03-29 15:09:16 -0300

    Implementando Login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,24 @@ class LoginController extends Controller
         return view('site.login', ['titulo' => 'Login']);
     }
 
-    public function autenticar(){
-        return 'chegamos até aqui';
+    public function autenticar(Request $request){
+        
+        //regras de validação
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        //mensagens de feedback de validação
+        $feedback = [
+            'usuario.email' => 'O campo usuário (e-mail) é obrigatório',
+            'senha.required' => 'O campo senha é obrigatorio'
+        ];
+
+        //validação, caso não validar redireciona para a rota
+        $request->validate($regras, $feedback);
+
+        print_r($request->all());
+
     }
 }
--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -13,8 +13,10 @@
                 <div style="width:33%; margin-left: 33%;"> 
                     <form action={{ route('site.login') }} method="post">
                         @csrf
-                        <input name="usuario" type="text" placeholder="Usuário" class="borda-preta">
-                        <input name="senha" type="password" placeholder="Senha" class="borda-preta">
+                        <input name="usuario" value="{{ old('usuario') }}" type="text" placeholder="Usuário" class="borda-preta">
+                        {{ $errors->has('usuario') ? $errors->first('usuario') : ''}}
+                        <input name="senha" value="{{ old('senha') }}"  type="password" placeholder="Senha" class="borda-preta">
+                        {{ $errors->has('senha') ? $errors->first('senha') : ''}}
                         <button type="submit" class="borda-preta">Acessar</button>
                     </form>
                 </div>
