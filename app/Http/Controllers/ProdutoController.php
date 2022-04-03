a32492bfdcf24f28dc9d91fad86a00ff84389009
PauloMoraiiis <paulodevmorais@gmail.com>
2022-04-03 13:23:01 -0300

    Ajuste Create/Edit produtos

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -108,6 +108,7 @@ class ProdutoController extends Controller
      */
     public function destroy(Produto $produto)
     {
-        //
+        $produto->delete();
+        return redirect()->route('produto.index');
     }
 }
