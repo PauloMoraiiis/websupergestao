3f171f859152abb0075fde8cd10b8aed2d427bf8
PauloMoraiiis <paulodevmorais@gmail.com>
2022-05-10 11:51:27 -0300

    Implementando hasMany

--- database/migrations/2022_05_06_201954_alter_produtos_relacionamento_fornecedores.php
+++ database/migrations/2022_05_06_201954_alter_produtos_relacionamento_fornecedores.php
@@ -14,18 +14,17 @@ class AlterProdutosRelacionamentoFornecedores extends Migration
     public function up()
     {
         //criando a coluna em produtos que vai receber a fk de fornecedores
-        Schema::table('produtos', function(Blueprint $table){
+        Schema::table('produtos', function(Blueprint $table) {
 
-            //insere o registro de fornecedor para estabelecer o relacionamento
+            //insere um registro de fornecedor para estabelecer o relacionamento
             $fornecedor_id = DB::table('fornecedores')->insertGetId([
                 'nome' => 'Fornecedor Padrão SG',
                 'site' => 'fornecedorpadraosg.com.br',
                 'uf' => 'SP',
-                'email' => 'contato@fornecedorpadraosg.com.br',
+                'email' => 'contato@fornecedorpadraosg.com.br'
             ]);
 
-
-            $table->unsignedBigInteger('fornecedor_id')->default('$fornecedor_id')->after('id');
+            $table->unsignedBigInteger('fornecedor_id')->default($fornecedor_id)->after('id');
             $table->foreign('fornecedor_id')->references('id')->on('fornecedores');
         });
     }
--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -26,6 +26,8 @@
                             <tr>
                                 <th>Nome</th>
                                 <th>Descrição</th>
+                                <th>Nome do Fornecedor</th>
+                                <th>Site do Fornecedor</th>
                                 <th>peso</th>
                                 <th>Unidade ID</th>
                                 <th>Comprimento</th>
@@ -37,7 +39,9 @@
                                 <tr>
                                     <td>{{ $produto->nome }}</td>
                                     <td>{{ $produto->descricao }}</td>
-                                    <td>{{ $produto->peso }}</td>
+                                    <td>{{ $produto->fornecedor->nome }}</td>
+                                    <td>{{ $produto->fornecedor->site }}</td>
+                                    <td>{{ $produto->peso }}</td>               
                                     <td>{{ $produto->unidade_id }}</td>
                                     <td>{{$produto->ItemDetalhe->comprimento ?? ''}}</td>
                                     <td>{{$produto->ItemDetalhe->altura ?? ''}}</td>
